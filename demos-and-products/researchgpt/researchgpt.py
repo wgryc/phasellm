26b0676a0b494a1e04fc587b2d62eb15300206ba
Wojciech Gryc <wojciech@gmail.com>
2023-04-25 00:54:10 -0400

    Update researchgpt.py

--- demos-and-products/researchgpt/researchgpt.py
+++ demos-and-products/researchgpt/researchgpt.py
@@ -1,39 +1,62 @@
+"""
+ResearchGPT, a demo by PhaseLLM
+See phasellm.com for more information
+
+The code below takes prompts uses a large language model (Claude, GPT-4, Cohere, etc.) to analyze your data and find insights.
+
+This file specifically uses Claude (Anthropic's LLM) and was tested on Claude v1.3.
+
+To see the full workflow, review the comments in the run_analysis() function.
+"""
+
 from phasellm.llms import ChatBot, ClaudeWrapper
 
+# Load API keys
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 MODEL = ClaudeWrapper(ANTHROPIC_API_KEY)
 CHATBOT = ChatBot(MODEL, "")
 
+# Used to print prompts and responses to screen
 DEBUG = True
 
+# Imports made available to the LLM for data analysis
+# Pandas is also used to load data to a data frame
 import pandas as pd
 import numpy as np 
 import sklearn as sklearn
 import scipy as scipy
 
+# The data frame to be analyzed
 df = None
-#df = pd.read_csv("nypd-motor-vehicle-collisions.csv")
-#df = pd.read_csv("incomes.csv")
 
+# Helper function to set the data frame via other Python files (e.g., see frontend.py)
 def set_df(new_df):
     global df 
     df = new_df
 
 def start_bi_session(): 
+    """
+    Starts the chat sesion. Instantiates a ChatBot object with the LLM referenced in the MODEL object at the top of the file. Starts a chat session with the prompt below, which confirms the ChatBot knows what to do.
+    """
+    
     prompt="""You are a data science helper and will be working with me to build a model to explore a data set. You do not need to provide qualifiers like "As an AI model" because I know you are such a model. I want you to be as productive and concise as possible with me.
 
 If you are generating code in a response, please limit your code generation to ONE (1) code block. If this means you need to add additional comments in the code, this is perfectly fine. Preceed every code block you generate with "|--START PYTHON CODE--|" and end each code block with "|--END PYTHON CODE--|".
 
 Do you understand? Please simply write "yes" if you do, and "no" with followup questions if you do not."""
     response = CHATBOT.chat(prompt)
-    if DEBUG: print(f"Understood? {response}")
-    assert response == "yes"
+    if DEBUG: print(f"Understood? {response}") # Should print "yes" if the LLM understands...
+    assert response == "yes" # ... otherwise will fail and error out.
     
 def ask_bi(msg):
+    """
+    Internal helper function. Generates code from the natural language query submitted.
+    
+    To see the full workflow, review the comments in the run_analysis() function.
+    """
     response = CHATBOT.chat(msg + " Please do not plot anything, just provide the Python code.")
     if DEBUG: print(response)
     p_start = response.find("|--START PYTHON CODE--|")
@@ -44,7 +67,8 @@ def ask_bi(msg):
         python_code = python_code.replace("\\n", "\n")
         if DEBUG: print(python_code)
     return python_code
-    
+
+# Imports to enable code execution and capturing outputs to a variable.
 # See: https://stackoverflow.com/questions/3906232/python-get-the-print-output-in-an-exec-statement    
 import sys
 from io import StringIO
@@ -52,6 +76,9 @@ import contextlib
 
 @contextlib.contextmanager
 def stdoutIO(stdout=None):
+    """
+    Used to hijack printing to screen so we can save the Python code output for the LLM.
+    """
     old = sys.stdout
     if stdout is None:
         stdout = StringIO()
@@ -60,6 +87,13 @@ def stdoutIO(stdout=None):
     sys.stdout = old
     
 def exec_code(code):
+    """
+    Executes arbitrary Python code and saves the output (or error!) to a variable.
+    
+    Returns the variable and a boolean (is_error) depending on whether an error took place.
+    
+    To see the full workflow, review the comments in the run_analysis() function.
+    """
     is_error = False
     with stdoutIO() as s:
         try:
@@ -70,6 +104,11 @@ def exec_code(code):
     return s.getvalue(), is_error
     
 def ask_interpret(code):
+    """
+    This function takes Python code and executes it, saving the output to the 'code_output' variable. It then passes both the code and the output to an LLM to ask it to interpret the results.
+    
+    ResearchGPT uses this to execute analysis on the dataframe 'df', and then asks the LLM to interpet the results.
+    """
     code_output, is_error = exec_code(code)
 
     if not is_error:
@@ -88,6 +127,17 @@ Could you please interpret this for me? Justify your answer by including the out
     return response, code_output
     
 def run_analysis(message):
+    """
+    Takes a message (i.e., natural language query) and uses the ChatBot object in this file to analyze the data given the natural language query. The function then returns the 'response_object', which contains the generated code, the raw output from the code, and an interpretation of the results.
+    
+    We also have a placeholder for error information. At this time, error messages from the Python code are included in the 'code_output' as technically they are the result of running the code.
+    
+    Here is how all the functions above work together:
+    (1) User submits a run_analysis(message) where the message is a natural language query.
+    (2) The message gets set to ask_bi(message), which gets converted into a proper LLM prompt. This then gets sent to the LLB ChatBot object, asking it to convert the natural language query to Python code.
+    (3) If Python coe is returned, then we send the Python code to ask_interpret(python_code). This function runs the Python code, takes the resulting output, and generates a new prompt -- one that asks the LLM ChatBot object to interpet the original code and the output of the code.
+    (4) The interpretation is then returned and included in the response_object.
+    """
     response_object = {"code":"*No code generated.*", "code_output":"*No output.*", "interpretation":"*No interpretation.*", "error":"*No error.*"}
     python_code = ask_bi(message)
     if DEBUG: print(f"\n\nPYTHON CODE:\n{python_code}\n\n")
