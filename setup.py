8a9f28c50e506affc29f223275582ef425d3d7ee
Wojciech Gryc <wojciech@gmail.com>
2023-04-25 01:59:00 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.0.2"
+VERSION = "0.0.3"
 
 DESCRIPTION = "Wrappers for common large langugae models (LLMs) with support for evaluation."
 
