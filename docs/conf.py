210b8c09dcfc3387e48db61b17c47c9cf59dc5b6
Wojciech Gryc <wojciech@gmail.com>
2023-04-27 01:42:04 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -7,6 +7,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../phasellm'))
 
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -18,15 +19,15 @@ release = '0.0.4'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
+extensions = ['autoapi.extension', 'sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+autoapi_dirs = ['../phasellm']
