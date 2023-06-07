0d8c18aee74279a943301f41f2904220603869ac
Garett MacGowan <garettsoftware@gmail.com>
2023-06-07 10:04:34 -0400

    Bump version to 0.0.10

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.0.9"
+VERSION = "0.0.10"
 
 DESCRIPTION = "Wrappers for common large langugae models (LLMs) with support for evaluation."
 
