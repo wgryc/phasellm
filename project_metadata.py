67d0346263c2b0555d2aa97f797ae5a61be9ba7c
Wojciech Gryc <wojciech@gmail.com>
2023-08-19 17:33:15 -0400

    Update project_metadata.py

--- project_metadata.py
+++ project_metadata.py
@@ -2,7 +2,7 @@ NAME = "phasellm"
 
 AUTHOR = "Wojciech Gryc"
 
-VERSION = "0.0.16"
+VERSION = "0.0.17"
 
 DESCRIPTION = "Wrappers for common large language models (LLMs) with support for evaluation."
 
