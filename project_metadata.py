42cd20ad98cf1a03fc16c7ec67988652997979d9
Wojciech Gryc <w@phaseai.com>
2023-10-16 11:29:40 -0400

    Adding readme + updating version #s.

--- project_metadata.py
+++ project_metadata.py
@@ -2,10 +2,14 @@ NAME = "phasellm"
 
 AUTHOR = "Wojciech Gryc"
 
-VERSION = "0.0.17"
+VERSION = "0.0.18"
 
-DESCRIPTION = "Wrappers for common large language models (LLMs) with support for evaluation."
+DESCRIPTION = (
+    "Wrappers for common large language models (LLMs) with support for evaluation."
+)
 
-LONG_DESCRIPTION = "PhaseLLM provides wrappers for common large language models and use cases. This makes it easy to " \
-                   "swap models in and out as needed. We also provide support for evaluation of models so you can " \
-                   "choose which models are better to use."
+LONG_DESCRIPTION = (
+    "PhaseLLM provides wrappers for common large language models and use cases. This makes it easy to "
+    "swap models in and out as needed. We also provide support for evaluation of models so you can "
+    "choose which models are better to use."
+)
